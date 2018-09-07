6242e00c19c5a9dc47a09d77044dca46dca34a35
CSchank <chris@schankula.ca>
2018-09-07 10:33:15 -0400

    change of how start looks in dot

--- Generate.hs
+++ Generate.hs
@@ -30,7 +30,7 @@ generateDot (startClient,startServer,clientState, serverState) =
         swaps = [("*CLIENTD", T.unlines (clientDiagram))
                 ,("*SERVERD", T.unlines (serverDiagram))
                 ,("*COMMD",   T.unlines (c2sDiagram ++ s2cDiagram))
-                ,("*START",   T.unlines $ T.concat ["\tstart->",s2GViz startClient,";"]:T.concat ["\tstart->",s2GViz startServer,";"]:[])
+                ,("*START",   T.unlines $ T.concat [s2GViz startClient,"[peripheries=2];"]:T.concat [s2GViz startServer,"[peripheries=2];"]:[])
                 ]
 
         clientList = M.toList clientState
@@ -57,7 +57,7 @@ clientStateToGViz ((cState0, cTrans), (cState1, mServe)) =
         case mServe of 
             Just m -> T.unlines $ 
                         T.concat ["\t\t",s2GViz cState0,"->",fakeName," [label=\"",s2GViz cTrans,"\",arrowhead=none];"] :
-                        T.concat ["\t\t",fakeName, " [shape=point, width=0, height=0]"] :
+                        T.concat ["\t\t",fakeName, " [shape=point, width=0, height=0];"] :
                         T.concat ["\t\t",fakeName,"->",s2GViz cState1,";"] : []
             Nothing -> T.concat ["\t\t",s2GViz cState0,"->",s2GViz cState1," [label=\"",s2GViz cTrans,"\"];"]
 
@@ -71,7 +71,7 @@ serverStateToGViz ((sState0, sTrans), (sState1, mClien)) =
         case mClien of
             Just m -> T.unlines $ 
                         T.concat ["\t\t",s2GViz sState0,"->",fakeName," [label=\"",s2GViz sTrans,"\",arrowhead=none];"] :
-                        T.concat ["\t\t",fakeName, " [shape=point, width=0, height=0]"] :
+                        T.concat ["\t\t",fakeName, " [shape=point, width=0, height=0];"] :
                         T.concat ["\t\t",fakeName,"->",s2GViz sState1,";"] : []
             Nothing -> T.concat ["\t\t",s2GViz sState0,"->",s2GViz sState1," [label=\"",s2GViz sTrans,"\"];"]
 
