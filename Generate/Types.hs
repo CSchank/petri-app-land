11ad7cfc9caf7cb076186e9b568829a8993d7437
CSchank <chris@schankula.ca>
2018-09-12 13:30:49 -0400

    generate Haskell types as well
    Though changes are small and this duplicates code, we may want to add records, etc later so keeping them separate makes sense

--- Generate/Types.hs
+++ Generate/Types.hs
@@ -33,3 +33,25 @@ elmCustomToClientType commentsEnabled (ElmCustom typeName constrs) =
         T.concat [ "type ", T.pack typeName, " =\n\t  "
                  , T.intercalate "\n\t| " constrs2Txt
                  ]
+
+elmCustomToServerType :: Bool -> ElmCustom -> T.Text
+elmCustomToServerType commentsEnabled (ElmCustom typeName constrs) =
+    let
+        constrs2Txt = map (\(constrName, elmDocTypes) -> T.intercalate " " $ T.pack constrName : map edt2Txt elmDocTypes) constrs
+
+        edt2Txt (et, n, d) = T.concat [et2Txt et, if commentsEnabled then T.concat [" {-", T.pack n, "-}"] else ""]
+
+        et2Txt ElmInt                       = "Int"
+        et2Txt (ElmIntRange _ _)            = "Int"
+        et2Txt ElmFloat                     = "Double"
+        et2Txt (ElmFloatRange _ _ _)        = "Double"
+        et2Txt ElmString                    = "String"
+        et2Txt (ElmSizedString _)           = "String"
+        et2Txt (ElmPair edt0 edt1)          = T.concat ["(",edt2Txt edt0,", ",edt2Txt edt1,")"]
+        et2Txt (ElmTriple edt0 edt1 edt2)   = T.concat ["(",edt2Txt edt0,", ",edt2Txt edt1,", ",edt2Txt edt2,")"]
+        et2Txt (ElmList edt)                = T.concat ["(List ",edt2Txt edt,")"]
+        et2Txt (ElmType name)               = T.pack name
+    in
+        T.concat [ "data ", T.pack typeName, " =\n\t  "
+                 , T.intercalate "\n\t| " constrs2Txt
+                 ]
