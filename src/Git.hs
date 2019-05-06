e5e51dc92e571880b0b3a7f6dfe7d08422932e4b
CSchank <chris@schankula.ca>
2019-05-06 12:00:52 -0400

    ability to update PAL

--- src/Git.hs
+++ src/Git.hs
@@ -9,6 +9,9 @@ import Codec.Archive.Zip
 import System.Directory
 import Control.Monad (when)
 import System.Console.ANSI
+import qualified Data.Text.IO as TIO
+import qualified Data.Text as T
+import Data.List
 
 newtype Zip = Zip String
     deriving (Show)
@@ -16,17 +19,33 @@ newtype Zip = Zip String
 instance FromJSON Zip where
     parseJSON = withObject "Zip" $ \ v -> Zip <$> v .: "zipball_url"
 
+newtype Release = Release String
+    deriving (Show)
+
+instance FromJSON Release where
+    parseJSON = withObject "Release" $ \ v -> Release <$> v .: "tag_name"
+
 requestWithUA :: String -> IO Request
 requestWithUA url = do
     initReq <- parseRequest url
     return $ setRequestHeader "user-agent" [""] $ setRequestSecure True initReq
 
+--getLatestRelease :: String -> IO Bytestring
+getLatestRelease repo = do
+    request <- requestWithUA $ "https://api.github.com/repos/" ++ repo ++ "/releases/latest"
+    rel <- httpLBS request
+    return $ getResponseBody rel
+
+getRelease rel repo = do
+    request <- requestWithUA $ "https://api.github.com/repos/" ++ repo ++ "/releases/" ++ rel
+    rel <- httpLBS request
+    return $ getResponseBody rel
+
 loadLatestTemplates :: IO ()
 loadLatestTemplates = do
-    request <- requestWithUA "https://api.github.com/repos/CSchank/PAL-templates/releases/latest"
-    print request
-    latestRelease <- httpLBS request
-    let mZip = decode $ getResponseBody latestRelease
+    latestVersion <- T.unpack <$> TIO.readFile ".palversion" --getLatestRelease "CSchank/PAL-templates"
+    latestRelease <- getRelease latestVersion "CSchank/PAL-templates"
+    let mZip = decode latestRelease
     case mZip of
         Just (Zip url) -> do
             zipReq <- requestWithUA url
@@ -41,4 +60,36 @@ loadLatestTemplates = do
             setSGR [SetColor Foreground Vivid Red]
             putStrLn "Error: Could not decode latest release from GitHub. You may have exceeded the API limit."
             putStrLn "Trying to fall back on last template downloaded..."
+            setSGR [Reset]
+
+-- from https://stackoverflow.com/a/5852820
+replaceNth :: Int -> a -> [a] -> [a]
+replaceNth _ _ [] = []
+replaceNth n newVal (x:xs)
+   | n == 0 = newVal:xs
+   | otherwise = x:replaceNth (n-1) newVal xs
+
+updatePAL :: IO ()
+updatePAL = do
+    latestRelease <- getLatestRelease "CSchank/petri-app-land"
+    let mRel = decode latestRelease
+    case mRel of
+        Just (Release rel) -> do
+            currentVersion <- T.unpack <$> TIO.readFile ".palversion"
+            if currentVersion == rel then do
+                putStrLn $ "PAL is already on the latest version (" ++ rel ++ ")"
+            else do
+                putStrLn $ "You are using an older version of PAL (" ++ currentVersion ++ "). Updating to version " ++ rel ++ "..."
+                stackYaml <- T.lines <$> TIO.readFile "stack.yaml"
+                case "#PALCOMMIT" `elemIndex` stackYaml of
+                    Just line -> do
+                        let newYaml = replaceNth (line+1) (T.concat["  commit: ",T.pack rel]) stackYaml
+                        TIO.writeFile "stack.yaml" $ T.unlines newYaml
+                        TIO.writeFile ".palversion" $ T.pack rel
+                        putStrLn $ "Update complete. Version is now " ++ rel ++ "."
+                    Nothing -> 
+                        return ()
+        Nothing -> do
+            setSGR [SetColor Foreground Vivid Red]
+            putStrLn "Error: Could not decode latest release from GitHub. You may have exceeded the API limit."
             setSGR [Reset]
